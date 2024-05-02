5d14801aa6316889cfd55ba15371d0ac9843950b
bhavy-0203 <146800987+bhavy-0203@users.noreply.github.com>
2024-05-02 13:08:16 +0530

    Add files via upload

---
+++ Task 3/3-1.js
@@ -0,0 +1,3 @@
+let name="Name: Bhavy Kakadiya";
+let age="Age: 16";
+console.log(name,"\n",age);
---
+++ Task 3/3-3.js
@@ -0,0 +1,7 @@
+let number = 10+58;
+
+console.log(number);
+
+number = 20+52;
+
+console.log(number);
