024c6a7598c86342fb72c8fb88800095d6f06025
bhavy-0203 <146800987+bhavy-0203@users.noreply.github.com>
2024-05-03 08:45:01 +0530

    Add files via upload

---
+++ Task SWAP/task 1.js
@@ -0,0 +1,11 @@
+let a=65,b=85,c;
+
+console.log("Before swapping");
+console.log(a,b);
+
+c=a;
+a=b;
+b=c;
+
+console.log("After swapping");
+console.log(a,b);
---
+++ Task SWAP/task 2.js
@@ -0,0 +1,11 @@
+let a=65,b=85;
+
+console.log("Before swapping");
+console.log(a,b);
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+console.log("After swapping");
+console.log(a,b);
